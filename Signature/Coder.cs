3b1e2b40ff8eafffc8a96c5744e0745c455c523e
ab.random <ab.random@yandex.ru>
2016-06-26 17:49:41 +0300

    First prototype, without threads and layers

---
+++ Signature/Coder.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signature
+{
+    class Coder
+    {
+        private readonly long _sizePart;   // размер части
+        private  readonly String _filePath;    // путь
+
+        // в конструкторе указываем корректные путь к файлу и размер части в байтах
+        public Coder(String path, long part)
+        {
+            _filePath = path;
+            _sizePart = part;
+        }
+
+        public void GetSignature()
+        {
+            int b = 0;  // переменная для считывания байт
+            byte[] buffer = new byte[_sizePart];    // массив для хранения байт в части
+            
+            using (FileStream codedFile = new FileStream(_filePath, FileMode.Open, FileAccess.Read))
+            {
+                for (long i = 0; i < GetCountOfParts(); i++)
+                {
+                    long position = codedFile.Seek(i*_sizePart, SeekOrigin.Begin);
+                    
+                    // для каждой части заполняем буфер
+                    for (long j = position; j < position + _sizePart; j++)
+                    {
+                        // читаем байт из пачки
+                        b = codedFile.ReadByte();
+                        // если он прочитан, то зибрасываем в буфер
+                        if (b != -1)
+                        {
+                            buffer[j - position] = (byte)b;
+                        }
+                        // иначе (в последней части байт может оказаться меньше, чем размер части)
+                        // достигли конец части и добиваем буфер нулями
+                        else
+                        {
+                            for (; j < position + _sizePart; j++)
+                            {
+                                buffer[j - position] = 0;
+                            }
+                        }
+                    }
+
+                    // тут работаем с заполненным буфером
+                    SHA256 mySha256 = SHA256.Create();
+                    Console.Write("Part N" + i + "\tSHA256: ");
+                    foreach (byte bt in mySha256.ComputeHash(buffer))
+                    {
+                        Console.Write(bt);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        // получаем количество частей
+        private long GetCountOfParts()
+        {
+            FileInfo codedFile = new FileInfo(_filePath);
+            long sizeFile = codedFile.Length;
+            long countOfParts = sizeFile/_sizePart;
+            
+            // если размер файла не кратен размеру части, то
+            // добавляем часть на отброшенный при делении остаток
+            if (sizeFile%_sizePart != 0)
+            {
+                countOfParts++;
+            }
+
+            return countOfParts;
+        }
+    }
+}
--- Signature/Program.cs
+++ Signature/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Signature {
-    class Program {
-        static void Main(string[] args) {
+namespace Signature
+{
+    class Program
+    {
+
+        private static long _sizePart;   // размер части
+        private static String _filePath;    // путь
+
+        static void Main(string[] args)
+        {
+            // вводим путь к файлу
+            Console.Write("Press enter the path of file: ");
+            _filePath = Console.ReadLine();
+
+            // выход, если файла не существует
+            if (!File.Exists(_filePath)) {
+                Console.WriteLine("File not found. Press any key to exit.");
+                Console.ReadKey();
+                return;
+            }
+
+            // вводим размер части
+            Console.Write("Press enter size of part in Byte: ");
+            
+            // выход, если некорректно ввели размер части
+            if (!Int64.TryParse(Console.ReadLine(), out _sizePart))
+            {
+                Console.WriteLine("Invalid size of part. Press any key to exit.");
+                Console.ReadKey();
+                return;
+            }
+
+            // создаём объект кодировщика и получаем сигнатуру файла
+            Coder coder = new Coder(_filePath, _sizePart);
+            coder.GetSignature();
+
+            
+
+            
+            Console.ReadKey();
+
+            
         }
     }
 }
